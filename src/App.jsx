6c90a6b335b69d7d745f903a4dd176a85e011cdb
barron <asd709380942@gmail.com>
2024-02-19 16:57:44 +0800

    feat: React 元件中的行內樣式設置

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Todo from "./components/Todo";
+import InlineComponent from "./components/inlineComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
-      <Todo />
+      {/* <Todo /> */}
+      <InlineComponent />
     </div>
   );
 }
---
+++ src/components/InlineComponent.jsx
@@ -0,0 +1,8 @@
+const header = { color: "blue", fontSize: "140px" };
+export default function InlineComponent() {
+  return (
+    <div>
+      <h1 style={header}>InlineComponent</h1>
+    </div>
+  );
+}
