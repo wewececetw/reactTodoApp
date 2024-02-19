34c1cc68dc9584ecb534c48102bb483a4e89d055
barron <asd709380942@gmail.com>
2024-02-19 14:31:05 +0800

    feat: 提交待辦事項

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 
 export default function Todo() {
   const [Todo, setTodo] = useState("");
+  const [Todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...Todos, Todo]);
+    setTodo("");
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           onChange={(e) => setTodo(e.target.value)}
@@ -12,6 +18,7 @@ export default function Todo() {
         />
         <button>Add</button>
       </form>
+      {console.log(Todos)}
     </div>
   );
 }
